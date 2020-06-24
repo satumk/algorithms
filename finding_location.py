115ed95cdea3794291cf8fccbf826a213962204e
satumk <satumm@gmail.com>
2020-06-24 19:43:38 +0300

    counting mean

---
+++ finding_location.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def mean(list_of_numbers) :
+    sum_of_list = 0
+    for i in list_of_numbers:
+        sum_of_list += i
+    
+    return sum_of_list/len(list_of_numbers)
+
